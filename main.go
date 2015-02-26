7c861aef5d44c6e8538f3fecd4fd4741b7e512e7
Stephen Waits <steve@waits.net>
2015-02-26 15:37:31 -0800

    minor tweaks to get closer to functional * bug: make parse() loop (was formerly a single select{} w/o a loop) * bug: make parse() send to hrefs chan from anon func to avoid DLing * misc: add specify chan directions in func params * misc: switch to "for var := range chan" where it makes sense

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ type page_data struct {
   count int
 }
 
-func download_manager(hrefs chan []string, parser chan *goquery.Document){
+func download_manager(hrefs <-chan []string, parser chan<- *goquery.Document){
   select {
   case links := <-hrefs:
     for _, url := range links {
@@ -21,38 +21,34 @@ func download_manager(hrefs chan []string, parser chan *goquery.Document){
   }
 }
 
-func downloader(url string, page chan *goquery.Document) {
+func downloader(url string, page chan<- *goquery.Document) {
   doc, _ := goquery.NewDocument(url)
-  //if err != nil {
-    //log.Fatal(err)
-  //}
-
-  page <- doc
+  if doc != nil {
+    page <- doc
+  }
 }
 
-//
 // Parse the specified __page__ for all links
 //  Send the number of links on that page to be analyzed to printer
 //  Send the links on the page to be downloaded
-func parse(page chan *goquery.Document, printer chan *page_data, hrefs chan []string){
+func parse(page <-chan *goquery.Document, printer chan<- *page_data, hrefs chan<- []string){
   links := make([]string, 0)
-  select {
-  case doc := <-page:
+  for doc := range page {
     doc.Find("a").Each(func(i int, s *goquery.Selection) {
       link, _ := s.Attr("href")
       links = append(links, link)
     })
 
-    hrefs <- links
-
-    result := &page_data{doc.Url.String(), len(links)}
-    printer <- result
+    go func() {
+      hrefs <- links
+      result := &page_data{doc.Url.String(), len(links)}
+      printer <- result
+    }()
   }
 }
 
-func printer(data chan *page_data){
-  for {
-    page_info := <-data
+func printer(data <-chan *page_data){
+  for page_info := range data {
     fmt.Printf("%s -> %d\n", page_info.url, page_info.count);
   }
 }
