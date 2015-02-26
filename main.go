a00c1e3a1f82de4126b7702aeff14e69e6ca57c4
Misha Manulis <misha@manulis.com>
2015-02-25 20:16:36 -0800

    Move download action into it's own go function
    - Use channels to sync downloader

--- main.go
+++ main.go
@@ -7,22 +7,32 @@ import (
   "github.com/PuerkitoBio/goquery"
 )
 
-func ExampleScrape() {
-  links := make(map[string]int)
-
+func downloader(url string, page chan *goquery.Document) {
   doc, err := goquery.NewDocument("http://news.ycombinator.com")
   if err != nil {
     log.Fatal(err)
   }
 
-  links["http://news.ycombinator.com"] = 0
-  doc.Find("td.title a").Each(func(i int, s *goquery.Selection) {
-    // s.Text()
-    links["http://news.ycombinator.com"]++
-    //fmt.Printf("Link: %s\n", name)
-  })
+  page <- doc
+}
 
-  fmt.Printf("%d\n", links["http://news.ycombinator.com"])
+func ExampleScrape() {
+  links := make(map[string]int)
+  page := make(chan *goquery.Document)
+
+  go downloader("http://news.ycombinator.com", page)
+
+  select {
+  case doc := <-page:
+    links["http://news.ycombinator.com"] = 0
+    doc.Find("td.title a").Each(func(i int, s *goquery.Selection) {
+      // s.Text()
+      links["http://news.ycombinator.com"]++
+      //fmt.Printf("Link: %s\n", name)
+    })
+
+    fmt.Printf("%d\n", links["http://news.ycombinator.com"])
+  }
 }
 
 func main() {
